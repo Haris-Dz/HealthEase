e3119cd3d3587d1241d66a2d3c6bf89876ca85ba
Haris Džoklo <55555552+Haris-Dz@users.noreply.github.com>
2025-05-18 01:46:40 +0200

    Implemented RabbitMQ e-mail sending

---
+++ HealthEase/HealthEase.Model/Messages/RegisterMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthEase.Model.Messages
+{
+    public class RegisterMessage
+    {
+        public string employeeFirstName { get; set; }
+        public string employeeLastName { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
